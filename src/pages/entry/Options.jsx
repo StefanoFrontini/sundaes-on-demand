54e0b43300ca6bdd22a18a52f4bead940ac617cb
Stefano Frontini <stefanofrontini75@gmail.com>
2023-09-10 10:46:47 +0200

    Test and code alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:3030/${optionType}`)
       .then((res) => res.json())
       .then((data) => setItems(data))
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
   }, [optionType]);
 
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
@@ -20,6 +26,9 @@ function Options({ optionType }) {
       imagePath={item.imagePath}
     />
   ));
+  if (error) {
+    return <AlertBanner />;
+  }
 
   return <Row>{optionItems}</Row>;
 }
