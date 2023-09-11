a8772e0879f5d3d465380a58e29a6543ca3cb322
Stefano Frontini <stefanofrontini75@gmail.com>
2023-09-11 12:40:04 +0200

    test and code context for scoops subtotal

---
+++ src/pages/summary/OrderSummary.jsx
@@ -0,0 +1,30 @@
+import { SummaryForm } from "./SummaryForm";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utilities";
+function OrderSummary() {
+  const { totals, optionsCount } = useOrderDetails();
+  const scoopsArray = Object.entries(optionsCount.scoops);
+  const scoopList = scoopsArray.map(([key, value]) => {
+    return (
+      <li key={key}>
+        {value} {key}
+      </li>
+    );
+  });
+  const toppingsArray = Object.keys(optionsCount.toppings);
+  const toppingList = toppingsArray.map((key) => {
+    return <li key={key}>{key}</li>;
+  });
+  return (
+    <div>
+      <h1>Order Summary</h1>
+      <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+      <ul>{scoopList}</ul>
+      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+      <ul>{toppingList}</ul>
+      <SummaryForm />
+    </div>
+  );
+}
+
+export default OrderSummary;
